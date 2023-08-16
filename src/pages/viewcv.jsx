69610a6b37b386ffa9fb627f61d61ea36c82944f
Noella Chartine <gikundiroHappy>
2023-08-15 22:19:26 -0700

    made viewcv page responsive

--- src/pages/viewcv.jsx
+++ src/pages/viewcv.jsx
@@ -5,6 +5,7 @@ import { TfiArrowCircleRight } from "react-icons/tfi";
 function Viewcv() {
   const [myCv, setMyCv] = useState([]);
 
+  const PDF_FILE_URL = "http://window.location.origin/file_pdf.pdf";
   const DisplayCv = () => {
     const item = localStorage.getItem("details");
     setMyCv(JSON.parse(item));
@@ -15,27 +16,38 @@ function Viewcv() {
     DisplayCv();
   }, []);
 
+  const downloadCvFile = (url) => {
+    const fileName = url.split("/").pop();
+    const aTag = document.createElement("a");
+    aTag.href = url;
+    aTag.setAttribute("download", "fileName");
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  };
+
   return (
-    <div
-      className="font-abc bg-gray-300 px-80 py-20
-     sm:bg-yellow-300 md:bg-green-300 lg:bg-pink-300 xl:bg-blue-300 2xl:bg-red-300 "
-    >
+    <div className="font-abc bg-gray-300 px-2 py-10 xl:px-80 lg:px-20 md:px-20 sm:px-10 ">
       {myCv.map((data) => (
         <div
-          className="bg-white rounded-sm textalign-center px-20 py-20 space-y-10"
+          className="bg-white rounded-sm textalign-center px-5 py-20 space-y-10 xl:px-20"
           key={data.id}
         >
           {" "}
           <div>
             <h1 className="font-bold">{data.fullName}</h1>
-            <div className="grid grid-cols-3 text-18 text-sm mt-3 lg:text-lg">
-              <div className="bg-red-100">
-                <p>{data.email}</p>
-                <span className="mr-2 ml-2 ">|</span>{" "}
+            <div className="grid grid-cols-2 sm:grid-cols-3 text-18 text-sm mt-3 lg:text-lg xl:text-sm">
+              <div className=" mb-3 flex justify-center">
+                <p>
+                  {data.email}
+                  <span className="mr-2 ml-2 ">|</span>
+                </p>{" "}
               </div>
-              <div className="bg-red-100">
-                <p> {data.phoneNumber} </p>
-                <span className="mr-2 ml-2">|</span>
+              <div className=" flex justify-center">
+                <p>
+                  {" "}
+                  {data.phoneNumber} <span className="mr-2 ml-2">|</span>
+                </p>
               </div>
               <div>
                 <p> {data.linkedin}</p>
@@ -52,7 +64,7 @@ function Viewcv() {
                 PROFESSIONAL SUMMARY
               </h1>
             </div>
-            <p className="mt-5 text-sm leading-6 lg:text-lg">
+            <p className="mt-5 text-sm leading-6 lg:text-lg md:text-lg sm:text-base xl:text-sm">
               {data.profession}
             </p>
           </div>
@@ -73,9 +85,9 @@ function Viewcv() {
                   {data.experience[0].position}
                 </h2>
               </div>
-              <div className="grid grid-cols-2 font-semibold">
-                <h3>{data.experience[0].company}</h3>
-                <p className="flex justify-end">
+              <div className="grid grid-cols-3 font-semibold">
+                <h3 className="col-span-1">{data.experience[0].company}</h3>
+                <p className="flex justify-end col-span-2">
                   <span className="  p-2 rounded-md">
                     {data.experience[0].starting}
                   </span>
@@ -85,7 +97,7 @@ function Viewcv() {
                   </span>
                 </p>
               </div>
-              <p className="text-sm leading-6 lg:text-lg">
+              <p className="text-sm leading-6 lg:text-lg md:text-lg sm:text-base xl:text-sm">
                 {data.experience[0].jobdescri}
               </p>
             </div>
@@ -99,9 +111,9 @@ function Viewcv() {
                       {data.experience[1].position}
                     </h2>
                   </div>
-                  <div className="grid grid-cols-2 font-semibold">
-                    <h3>{data.experience[1].company}</h3>
-                    <p className="flex justify-end">
+                  <div className="grid grid-cols-3 font-semibold ">
+                    <h3 className="col-span-1">{data.experience[1].company}</h3>
+                    <p className="flex justify-end col-span-2">
                       <span className="p-2 rounded-md">
                         {data.experience[1].starting}
                       </span>
@@ -111,7 +123,7 @@ function Viewcv() {
                       </span>
                     </p>
                   </div>
-                  <p className="text-sm leading-6 lg:text-lg">
+                  <p className="text-sm leading-6 lg:text-lg md:text-lg sm:text-base xl:text-sm">
                     {data.experience[1].jobdescri}
                   </p>
                 </div>
@@ -137,7 +149,7 @@ function Viewcv() {
               </p>
             </div>
             <h2 className="font-semibold">{data.education[0].degree}</h2>
-            <p className="text-sm leading-6 lg:text-lg">
+            <p className="text-sm leading-6 lg:text-lg md:text-lg sm:text-base xl:text-sm">
               {data.education[0].institution}
             </p>
 
@@ -153,7 +165,7 @@ function Viewcv() {
                     </p>
                   </div>
                   <h2 className="font-semibold">{data.education[1].degree}</h2>
-                  <p className="text-sm leading-6 lg:text-lg">
+                  <p className="text-sm leading-6 lg:text-lg md:text-lg sm:text-base xl:text-sm">
                     {data.education[1].institution}
                   </p>
                 </div>
@@ -161,7 +173,12 @@ function Viewcv() {
             </div>
           </div>
           <div className="flex justify-end">
-            <button className="bg-green-700 px-5 py-3 font-bold rounded-md text-white hover:border-green-700 hover:border-opacity-75 hover:bg-white hover:text-green-800 hover:border-2">
+            <button
+              className="bg-green-700 px-5 py-3 font-bold rounded-md text-white hover:border-green-700 hover:border-opacity-75 hover:bg-white hover:text-green-800 hover:border-2"
+              onClick={() => {
+                // downloadCvFile(PDF_FILE_URL);
+              }}
+            >
               Download Cv
             </button>
           </div>
